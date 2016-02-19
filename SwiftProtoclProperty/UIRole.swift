b0648e90d527491feac8293e6ce43c00ba39aae6
YoungChen <chainone@gmail.com>
2016-02-19 16:31:07 +0800

    Just change comments

--- SwiftProtoclProperty/UIRole.swift
+++ SwiftProtoclProperty/UIRole.swift
@@ -24,7 +24,7 @@ func ==(lhs: UIRole, rhs: UIRole) -> Bool {
     //
     //FIXME:  Is it a swift bug?
     // If I comment ```&& lhs.hasAssignee == rhs.hasAssignee``` here, compile will report error
-    // However, if I put code (UIRole, UICompany, Assigneeable) together, it compiles
+    // However, if I put code in UIRole.swift, Assigneeable.swift into one single Swift file , it compiles
     //
     
     return lhs.oxygenId == rhs.oxygenId && lhs.name == rhs.name /*&& lhs.hasAssignee == rhs.hasAssignee*/
